35c837bafe7f648bf2096909fa2e26a07f46877a
Eli Cleveland <elicleveland12@gmail.com>
2019-01-03 04:35:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,12 @@ def key_for_min_value(name_hash)
   if name_hash = {}
     return nil
   end
-    min_value = name_hash.first[1]
-    min_key = name_hash.first[0]
-    name_hash.each do |key, value|
-      if value < min_value
-        min_value = value
-        min_key = key
-      end
+  i = 0 
+  name_hash.each do |key, value|
+     
+    if value[i] - value[i + 1]
+      
     end
-  min_key
+    i += 1 
+      
 end
