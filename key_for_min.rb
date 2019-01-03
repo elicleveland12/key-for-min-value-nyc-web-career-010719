d21541dfaf9517a05318c3bcfb2efe8d25e2f1e5
Eli Cleveland <elicleveland12@gmail.com>
2019-01-03 04:48:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,5 @@ def key_for_min_value(name_hash)
   if name_hash = {}
     return nil
   end
-  #i = 0 
-  name_hash.each do |key, value|
-    if name_hash.length > (i + 1) 
-      value[i] 
-    #i += 1 
-      
+  
 end
